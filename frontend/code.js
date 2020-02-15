16cf9259aad473c9e60bef62a100cf3ae854580e
Joshua Maros <jmaros512@gmail.com>
2020-02-15 00:11:23 -0800

    Created simple on-screen geometry.

--- frontend/code.js
+++ frontend/code.js
@@ -1,12 +1,39 @@
 require([
     "esri/Map",
-    "esri/views/MapView"
-], function(Map, MapView) {
-    var map = new Map({
-        basemap: "topo-vector"
+    "esri/views/MapView",
+    "esri/layers/GraphicsLayer",
+    "esri/Graphic",
+], (Map, MapView, GraphicsLayer, Graphic) => {
+    let graphic = new Graphic({
+        geometry: {
+            type: 'polyline',
+            paths: [
+                [-118.7, 34.0],
+                [-118.8, 34.0],
+                [-118.8, 34.1],
+            ]
+        },
+        symbol: {
+            type: 'simple-line',
+            color: [255, 0, 255],
+            width: 4
+        },
+        attributes: {
+            colorDescription: 'obnoxious',
+            polishLevel: 'negative',
+        }
     });
 
-    var view = new MapView({
+    let layer = new GraphicsLayer({
+        graphics: [graphic]
+    });
+
+    let map = new Map({
+        basemap: "satellite"
+    });
+    map.add(layer);
+
+    let view = new MapView({
         container: "viewDiv",
         map: map,
         center: [-118.80500, 34.02700], // longitude, latitude

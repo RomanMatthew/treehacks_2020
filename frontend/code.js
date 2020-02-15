9f5a3d3ff5b9e0f825b160b8009fbb2e9bfd2873
Joshua Maros <jmaros512@gmail.com>
2020-02-15 02:56:11 -0800

    Switched to rendering using the more robust FeatureLayer object.

--- frontend/code.js
+++ frontend/code.js
@@ -1,9 +1,9 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/GraphicsLayer",
+    "esri/layers/FeatureLayer",
     "esri/Graphic",
-], (Map, MapView, GraphicsLayer, Graphic) => {
+], (Map, MapView, FeatureLayer, Graphic) => {
     let graphic = new Graphic({
         geometry: {
             type: 'polyline',
@@ -13,19 +13,25 @@ require([
                 [-118.8, 34.1],
             ]
         },
-        symbol: {
-            type: 'simple-line',
-            color: [255, 0, 255],
-            width: 4
-        },
         attributes: {
-            colorDescription: 'obnoxious',
-            polishLevel: 'negative',
+            _objectId: 0,
         }
     });
 
-    let layer = new GraphicsLayer({
-        graphics: [graphic]
+    let layer = new FeatureLayer({
+        source: [graphic],
+        objectIdField: '_objectId',
+        fields: [
+            { name: '_objectId', type: 'oid' },
+        ],
+        renderer: {
+            type: 'simple',
+            symbol: {
+                type: 'simple-line',
+                color: [255, 0, 255],
+                width: 4,
+            }
+        }
     });
 
     let map = new Map({
